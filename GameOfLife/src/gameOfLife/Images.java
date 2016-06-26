91e46527d60dd116049a2f5570832d5dba116ea2
Brennen <brennendavis@gmail.com>
2016-06-25 22:29:49 -0600

    Initial Commit of Game of Life

---
+++ GameOfLife/src/gameOfLife/Images.java
@@ -0,0 +1,36 @@
+/*
+ * Brennen Davis and Robert Davis
+ * Spring 1410 Team Project
+ */
+package gameOfLife;
+
+import javax.swing.ImageIcon;
+
+public enum Images {
+	PLAY("Play.png"),
+	PAUSE("Pause.png"),
+	RESET("Reset.png"),
+	INSTRUCTIONS("Instructions.png"),
+	RANDOM("Random.png"),
+	MENU("Menu.png"),
+	LOAD("Load.png"),
+	SAVE("Save.png"),
+	NRANDOM("NRandom.png"),
+	DEMO("GoLDemo.gif");
+	
+	private ImageIcon image;
+	
+	Images(String imagePath){
+		this.image = new ImageIcon(GameOfLifeGui.class.getResource("/gameOfLife/images/"+imagePath));
+	}
+
+	/**
+	 * @return the image
+	 */
+	public ImageIcon getImage() {
+		return image;
+	}
+	
+	
+
+}
